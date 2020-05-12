2803cf7cbfa386204f2963947c34bdc265429faa
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-12 00:37:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,21 +1,4 @@
-badge_maker(Edsger)
-puts "Hello, my name is Edsger"
-
-badge_maker(Ada)
-puts "Hello, my name is Ada"
-
-badge_maker(Charles)
-puts "Hello, my name is Charles"
-
-badge_maker(Alan)
-puts "Hello, my name is Alan"
-
-badge_maker(Grace)
-puts "Hello, my name is Grace"
-
-badge_maker(Linus)
-puts "Hello, my name is Linus"
-
-def batch_badge_creator
-  names = ("Edsger", "Ada", "Charles", "Alan", "Grace", "Linus")
+def badge_maker(name)
+  "Hello, my name is #{name}."
 end
+def assign_rooms
