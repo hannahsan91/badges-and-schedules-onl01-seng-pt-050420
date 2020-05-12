6bb2a5801df903ea5f99c7dd4de362e9da8cb770
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-12 00:35:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,2 +1,17 @@
 badge_maker(Edsger)
 puts "Hello, my name is Edsger"
+
+badge_maker(Ada)
+puts "Hello, my name is Ada"
+
+badge_maker(Charles)
+puts "Hello, my name is Charles"
+
+badge_maker(Alan)
+puts "Hello, my name is Alan"
+
+badge_maker(Grace)
+puts "Hello, my name is Grace"
+
+badge_maker(Linus)
+puts "Hello, my name is Linus"
